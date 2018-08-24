80dc7e5c283f709c76843c7817c05ecd8fee4567
Abrams88 <37910841+Abrams88@users.noreply.github.com>
2018-08-24 14:10:12 +0100

    Update jenkins_server.sh

--- terrascripts/jenkins_server.sh
+++ terrascripts/jenkins_server.sh
@@ -2,7 +2,7 @@
 sudo yum install -y git
 sudo yum install -y wget nano 
 sudo yum install -y epel-release
-sudo yum update
+sudo yum update -y
 sudo useradd jenkins
 sudo yum install -y java-1.8.0-openjdk.x86_64
 cd ~ && sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
